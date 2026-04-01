7fcc9b1e51130469464da31bbaa19321fed6e33b
KGO <korast@naver.com>
2026-04-01 19:33:52 +0900

    Update WidgetController, OverlayWidgetController, Setting HP, HUD

--- Source/Aura/Public/UI/Widget/OverlayWidgetController.h
+++ Source/Aura/Public/UI/Widget/OverlayWidgetController.h
@@ -6,12 +6,24 @@
 #include "UI/Widget/AuraWidgetController.h"
 #include "OverlayWidgetController.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangeSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangeSignature, float, NewMaxHealth);
+
 /**
- * 
+ *
  */
-UCLASS()
+UCLASS(BlueprintType, Blueprintable)
 class AURA_API UOverlayWidgetController : public UAuraWidgetController
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void BroadcastInitialValves() override;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnHealthChangeSignature OnHealthChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnMaxHealthChangeSignature OnMaxHealthChanged;
+
 };
