cdc84f226416a2f29c4d97bd11188f698b51d57d
KGO <korast@naver.com>
2026-04-06 10:45:12 +0900

    Create GameplayTag Data Table, Bind GameplayTag Message Event

--- Source/Aura/Private/UI/Widget/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/Widget/OverlayWidgetController.cpp
@@ -4,6 +4,7 @@
 #include "UI/Widget/OverlayWidgetController.h"
 
 #include "AttributeSet.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 
 void UOverlayWidgetController::BroadcastInitialValves()
@@ -33,6 +34,22 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 
 	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(
 		AuraAttributeSet->GetMaxManaAttribute()).AddUObject(this, &UOverlayWidgetController::MaxManaChanged);
+
+	Cast<UAuraAbilitySystemComponent>(AbilitySystemComponent)->EffectAssetTags.AddLambda(
+		[this](const FGameplayTagContainer& AssetTags)
+		{
+			for (const FGameplayTag& AssetTag : AssetTags)
+			{
+				//expanding out parent tags "A.1".MatchesTag("A") will return True, "A".MatchesTag("A.1") will return False
+				//해당 Tag가 포함되어 있는가를 확인할 때, 상위 계층의 태그를 검색하면 True를 반환하지만, 하위 계층의 태그는 false 반환
+				FGameplayTag MessageTag = FGameplayTag::RequestGameplayTag(FName("Message"));
+				if (AssetTag.MatchesTag(MessageTag))
+				{
+					FUIWidgetRow* Row = GetDataTableRowByTag<FUIWidgetRow>(MessageWidgetDataTable, AssetTag);
+					MessageWidgetRowDelegate.Broadcast(*Row);
+				}
+			}
+		});
 }
 
 void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
