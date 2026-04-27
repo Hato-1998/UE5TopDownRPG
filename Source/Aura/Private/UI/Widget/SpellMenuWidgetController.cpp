cdbae1887d0a59cf122a9f41e239fa16d9ecbe49
KGO <korast@naver.com>
2026-04-27 21:51:47 +0900

    Create SpellMenu UI Widget
    스킬 세팅하는 UI 작업

---
+++ Source/Aura/Private/UI/Widget/SpellMenuWidgetController.cpp
@@ -0,0 +1,5 @@
+// CopyrightHATO
+
+
+#include "UI/Widget/SpellMenuWidgetController.h"
+
---
+++ Source/Aura/Public/UI/Widget/SpellMenuWidgetController.h
@@ -0,0 +1,17 @@
+// CopyrightHATO
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/Widget/AuraWidgetController.h"
+#include "SpellMenuWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API USpellMenuWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
